3e721d755bd86feb628e9ae3ea3064aa014a40d9
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-02 11:19:37 +0200

    add sql db source code

---
+++ back/db/init.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+CREATE TABLE registered_user (
+    "userId" SERIAL PRIMARY KEY,
+    "fullName" VARCHAR(100) NOT NULL UNIQUE,
+    "userName" VARCHAR(100) NOT NULL UNIQUE,
+    "password" VARCHAR(500) NOT NULL,
+    "emailAddress" VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE patients (
+    "pacientId" SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL,
+    "date" DATE NOT NULL,
+    "description" TEXT NOT NULL,
+    "userId" INT NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY ("userId")
+        REFERENCES registered_user("userId")
+        ON DELETE CASCADE
+);
+
+CREATE TABLE "admin" (
+"adminId" SERIAL PRIMARY KEY,
+"fullName" VARCHAR(50) NOT NULL UNIQUE
+);
+
+END;
