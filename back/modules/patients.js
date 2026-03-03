787a4a183626e21ac6e46e27143c639ce33997ff
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-03 14:38:49 +0200

    add search patients by name, add sorte by name ASC, DESC, and date ASC, DESC

--- back/modules/patients.js
+++ back/modules/patients.js
@@ -20,10 +20,22 @@ export const postNewPatientM = async (newData, { id }) => {
 
 // get all patients by user id
 
-export const getAllPatientsByIdM = async (userId, name) => {
-  return await sql`
+export const getAllPatientsByIdM = async (userId, name, sort) => {
+  const sortMap = {
+    DESC: sql`name DESC`,
+    ASC: sql`name ASC`,
+    DATEDESC: sql`date ASC`,
+    DATEASC: sql`date DESC`,
+  };
+
+  const orderBy = sortMap[sort] || sql``;
+
+  const patientsList = sql`
 SELECT * FROM patients
 WHERE "userId" = ${Number(userId)}
 ${name ? sql`AND name ILIKE ${`%` + name + `%`}` : sql``}
+${sort ? sql`ORDER BY ${orderBy}` : sql``}
 `;
+
+  return patientsList;
 };
