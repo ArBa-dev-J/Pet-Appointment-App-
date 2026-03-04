760899397f1480bf01e6c22f1b9e66592fbd9b7c
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-04 09:53:31 +0200

    add validate for patients

--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -14,6 +14,7 @@ import {
 import validate from "../validator/validate.js";
 import validateNewUser from "../validator/validateNewUser.js";
 import validateNewLogin from "../validator/validateLogin.js";
+import validatePatient from "../validator/validatePatient.js";
 
 const userRoutes = express.Router();
 // user registration
@@ -24,10 +25,16 @@ userRoutes.route("/login").get(validateNewLogin, validate, loginC);
 // user patients
 userRoutes
   .route("/:id/patients/new")
-  .post(protect, allowAccessTo("user"), restrictToOwnUser, postNewPatientC);
+  .post(
+    protect,
+    allowAccessTo("user"),
+    restrictToOwnUser,
+    validatePatient,
+    validate,
+    postNewPatientC,
+  );
 userRoutes
   .route("/:id/patients")
   .get(protect, allowAccessTo("user"), restrictToOwnUser, getAllpatientsByIdC);
 
 export default userRoutes;
- 
---
+++ back/validator/validatePatient.js
@@ -0,0 +1,28 @@
+import { body } from "express-validator";
+
+const validatePatient = [
+  body().notEmpty().withMessage("Must contain data"),
+
+  body("name")
+    .isString()
+    .withMessage("Name must be a string")
+    .trim()
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Must be from 2 to 100 chars longs"),
+
+    body("date")
+    .isString()
+    .withMessage("Name must be a string")
+    .isDate()
+    .withMessage("must be a date"),
+
+    body("description")
+    .isString()
+    .withMessage("Name must be a string")
+    .trim()
+    .isLength({ min: 2, max: 1000 })
+    .withMessage("Must be from 2 to 100 chars longs"),
+];
+
+
+export default validatePatient;
