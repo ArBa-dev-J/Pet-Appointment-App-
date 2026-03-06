b779c178c0a4485adc7d1a31b939666d8ab6c648
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-06 10:23:47 +0200

    add login registres

--- front/src/components/LoginForm.jsx
+++ front/src/components/LoginForm.jsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router";
+import { useForm } from "react-hook-form";
 
 function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(JSON.stringify(data));
+    reset();
+  };
+
   return (
     <>
       <main>
@@ -11,17 +24,44 @@ function LoginForm() {
         </header>
         <section>
           <h1 className="pt-50 text-center">Login form</h1>
-          <form className="flex flex-col justify-center items-center pt-10 gap-2">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col justify-center items-center pt-10 gap-2"
+          >
             <label>Your Email</label>
             <input
               type="text"
+              {...register("emailAddress", {
+                required: true,
+                minLength: 5,
+                maxLength: 30,
+                pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              })}
               className="border block"
               placeholder="example@email.com"
             />
+            {errors.emailAddress && (
+              <span>
+                Must contain your email and must be from 5 to 30 characters long
+              </span>
+            )}
 
             <label>Your password</label>
-            <input type="password" className="border block" />
-
+            <input
+              type="password"
+              {...register("password", {
+                required: true,
+                minLength: 2,
+                maxLength: 100,
+              })}
+              className="border block"
+            />
+            {errors.emailAddress && (
+              <span>
+                Must contain your password and must be from 2 to 100 characters
+                long
+              </span>
+            )}
             <input type="submit" value="Login" />
           </form>
         </section>
