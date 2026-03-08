5552a09c669195990412918593d86c805b35ac98
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-08 10:27:08 +0200

    add nav from new appointment from to main user page

--- front/src/components/forms/PatientApForm.jsx
+++ front/src/components/forms/PatientApForm.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router";
+import { UserContext } from "../../contexts/UserContext";
+import { useContext } from "react";
+
+
 function PatientApForm(){
+    const user = useContext(UserContext);
 return(
     <>
-    
+    <main>
+        <header className="text-center">
+           <Link to={`/user/${user.user.data.data.userId}/apointments`}><h1 className="pt-10">Pet Clinic</h1></Link>
+        </header>
+    </main>
     </>
 );
 }
