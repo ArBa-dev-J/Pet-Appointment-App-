775463cc3273388fc066d2078d52d83a6df0f507
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-09 08:01:04 +0200

    add register form for new appointments

--- front/src/components/forms/PatientApForm.jsx
+++ front/src/components/forms/PatientApForm.jsx
@@ -1,34 +1,83 @@
 import { Link } from "react-router";
 import { UserContext } from "../../contexts/UserContext";
 import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
 
 function PatientApForm() {
-    const user = useContext(UserContext);
-    return (
-        <>
-            <main>
-                <header className="text-center">
-                    <Link to={`/user/${user.user.data.data.userId}/apointments`}><h1 className="pt-10">Pet Clinic</h1></Link>
-                </header>
-                <section>
-                    <h1 className="text-center pt-50">New Appointment</h1>
-                    <form className="flex flex-col justify-center items-center pt-10 gap-2">
-                        <label>Appoitment title</label>
-                        <input type="text" className="block border" />
+  let navigate = useNavigate();
+  //   const [error, setError] = useState(null);
+  const user = useContext(UserContext);
 
-                        <label>Appointment date</label>
-                        <input type="datetime-local" className="block border" />
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      isConfirmed: false,
+    },
+  });
 
-                        <label>Appointment description</label>
-                        <input type="text" className="block border" />
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-                        <input type="submit" value="Put new appointment" />
-                    </form>
-                </section>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <header className="text-center">
+          <Link to={`/user/${user.user.data.data.userId}/apointments`}>
+            <h1 className="pt-10">Pet Clinic</h1>
+          </Link>
+        </header>
+        <section>
+          <h1 className="text-center pt-50">New Appointment</h1>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col justify-center items-center pt-10 gap-2"
+          >
+            <label>Appoitment title</label>
+            <input
+              type="text"
+              {...register("name", {
+                required: true,
+                minLength: 2,
+                maxLength: 50,
+              })}
+              className="block border"
+            />
+
+            <label>Appointment date</label>
+            <input
+              type="datetime-local"
+              {...register("date", {
+                required: true,
+              })}
+              className="block border"
+            />
+
+            <label>Appointment description</label>
+            <input
+              type="text"
+              {...register("description", {
+                required: true,
+                minLength: 2,
+                maxLength: 1000,
+              })}
+              className="block border"
+            />
+
+            <input type="submit" value="Put new appointment" />
+          </form>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default PatientApForm;
