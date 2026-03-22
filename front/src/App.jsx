de85cda9f54ec6b99aa14287e449b923a146df33
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-22 07:09:30 +0200

    add useState , useContect imports to App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router";
+import { useContext, useState } from "react";
+import { AppointmentsContext } from "./contexts/AppointmetsContext";
 import Home from "./components/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
 import SignUpForm from "./components/forms/SignUpForm";
@@ -8,41 +10,45 @@ import UserPage from "./components/user/UserPage";
 import PatientApForm from "./components/forms/PatientApForm";
 
 function App() {
+  const appointments = useContext(AppointmentsContext);
+  const [selectedAppointment, setSelectedAppointment] = useState(null);
+
+  
   return (
-   <>
-   <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/SignUp" element={<SignUpForm/>}/>
-      <Route path="/Login" element={<LoginForm/>}/>
-      
-      <Route
-        path="/user/:userId/apointments"
-        element={
-          <ProtectedRoute>
-            <UserPage/>
-          </ProtectedRoute>
-        }
-      />
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/SignUp" element={<SignUpForm />} />
+        <Route path="/Login" element={<LoginForm />} />
 
-      <Route
-        path="/user/:userId/apointments/new"
-        element={
-          <ProtectedRoute>
-            <PatientApForm/>
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/user/:userId/apointments"
+          element={
+            <ProtectedRoute>
+              <UserPage />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/user/:userId/apointments/update"
-        element={
-          <ProtectedRoute>
-            <UpdateForm/>
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
-   </>
+        <Route
+          path="/user/:userId/apointments/new"
+          element={
+            <ProtectedRoute>
+              <PatientApForm />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/user/:userId/apointments/update"
+          element={
+            <ProtectedRoute>
+              <UpdateForm />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
   )
 }
 
